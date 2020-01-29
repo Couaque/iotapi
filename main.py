06995dd5b5bd104bce8a56f1051acbc098fba4c8
Téo Hays <teo@couaque.fr>
2020-01-29 21:58:19 +0000

    PING feature moved to a different file. One feature will correspond to one file in the future.

--- main.py
+++ main.py
@@ -5,21 +5,4 @@ from subprocess import check_output
 
 app = Flask(__name__)
 
-
-@app.route('/ping/<target>')
-def ping(target):
-    try:
-        print("RUNNING : ping -c4 " + target)
-        completed = subprocess.run("ping -c4 " + target, shell=True, stdout=subprocess.PIPE)
-
-        output = output = completed.stdout.decode('utf-8')
-        print(output)
-        print('returncode:', completed.returncode)
-
-        output = output.split("\n")
-        
-    except subprocess.CalledProcessError as err:
-        print('ERROR:', err)
-        output = "ERROR: " + err
-
-    return Response(ujson.dumps(output), mimetype="application/json")
+import ping
---
+++ ping.py
@@ -0,0 +1,21 @@
+import subprocess, ujson
+from flask import Response
+from main import app
+
+@app.route('/ping/<target>')
+def ping(target):
+    try:
+        print("RUNNING : ping -c4 " + target)
+        completed = subprocess.run("ping -c4 " + target, shell=True, stdout=subprocess.PIPE)
+
+        output = output = completed.stdout.decode('utf-8')
+        print(output)
+        print('returncode:', completed.returncode)
+
+        output = output.split("\n")
+        
+    except subprocess.CalledProcessError as err:
+        print('ERROR:', err)
+        output = "ERROR: " + err
+
+    return Response(ujson.dumps(output), mimetype="application/json")
