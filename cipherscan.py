d11790f3082b9b67850fc646272cd49ebddff754
Téo Le Dozo <teo@couaque.fr>
2020-02-20 16:03:14 +0000

    Modified the parsing for the cipherscan. Need more tweaks for it to be easily usable.

--- cipherscan.py
+++ cipherscan.py
@@ -1,5 +1,5 @@
 #Usual imports
-import subprocess, ujson
+import subprocess, ujson, re, xmltodict
 from flask import Response
 from app import app
 
@@ -8,8 +8,8 @@ from app import app
 def cipherscan(target):
     try:
         #We run the nmap script inside our Linux machine.
-        print("RUNNING : nmap -sV --script ssl-enum-ciphers -p 443 " + target)
-        completed = subprocess.run("nmap -sV --script ssl-enum-ciphers -p 443 " + target, shell=True, stdout=subprocess.PIPE)
+        print("RUNNING : nmap -oX - -sV --script ssl-enum-ciphers -p 443 " + target)
+        completed = subprocess.run("nmap -oX - -sV --script ssl-enum-ciphers -p 443 " + target, shell=True, stdout=subprocess.PIPE)
 
         #We decode the output as UTF-8...
         output = completed.stdout.decode('utf-8')
@@ -21,8 +21,25 @@ def cipherscan(target):
         #To make this easier I split the single string corresponding with the console result
         #In an array of multiple lines, by spiting the string in a new array cell every time
         #there is a return character, \n
-        output = output.split("\n")
-        
+        output = xmltodict.parse(output)
+
+
+        """
+        tmp=[]
+        p = re.compile("<elem key=\"name\">|<elem key=\"strength\"|<elem>!(NULL)")
+        for line in output:
+            #If the regex matches the line...
+            if type(p.match(line)) == re.Match :
+                #...We add it to tmp
+                tmp.append(line)
+        output = []
+
+        for line in tmp:
+            #trimmed_line = line[21:-7]
+            output.append(line)
+        """
+
+
     #If the process call goes wrong for some reason, we raise an exception.
     #This allows us to keep the program running.
     except subprocess.CalledProcessError as err :
