6f14cb71025f928a0d1a8a151ff26c332151235a
Téo Le Dozo <teo@couaque.fr>
2020-03-03 10:32:13 +0000

    Added Mozilla's cipherscan and tested the JSON response. Modified the install script to add all the dependencies needed. Just run it to update.

--- cipherscan.py
+++ cipherscan.py
@@ -9,7 +9,7 @@ def cipherscan(target):
     try:
         #We run the nmap script inside our Linux machine.
         print("RUNNING : nmap -oX - -sV --script ssl-enum-ciphers -p 443 " + target)
-        completed = subprocess.run("nmap -oX - -sV --script ssl-enum-ciphers -p 443 " + target, shell=True, stdout=subprocess.PIPE)
+        completed = subprocess.run("../cipherscan/cipherscan -j --curves " + target, shell=True, stdout=subprocess.PIPE)
 
         #We decode the output as UTF-8...
         output = completed.stdout.decode('utf-8')
@@ -18,28 +18,6 @@ def cipherscan(target):
         print(output)
         print('returncode:', completed.returncode)
 
-        #To make this easier I split the single string corresponding with the console result
-        #In an array of multiple lines, by spiting the string in a new array cell every time
-        #there is a return character, \n
-        output = xmltodict.parse(output)
-
-
-        """
-        tmp=[]
-        p = re.compile("<elem key=\"name\">|<elem key=\"strength\"|<elem>!(NULL)")
-        for line in output:
-            #If the regex matches the line...
-            if type(p.match(line)) == re.Match :
-                #...We add it to tmp
-                tmp.append(line)
-        output = []
-
-        for line in tmp:
-            #trimmed_line = line[21:-7]
-            output.append(line)
-        """
-
-
     #If the process call goes wrong for some reason, we raise an exception.
     #This allows us to keep the program running.
     except subprocess.CalledProcessError as err :
@@ -47,4 +25,4 @@ def cipherscan(target):
         output = "ERROR: " + completed.stdout.decode('utf-8')
 
     #We return an HTTP response anyway, error or not.
-    return Response(ujson.dumps(output), mimetype="application/json")
+    return Response(output, mimetype="application/json")
--- install.sh
+++ install.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
+echo "Please run this script with yavs' folder as your current folder !"
 
 echo "Installing Python3 and PIP from APT..."
-apt install python3 python3-pip nmap -y
+apt install python3 python3-pip nmap python python-pip git -y
 
-echo "Installing Python modules from pip repos..."
+echo "Installing Python modules from pip3 repos..."
 pip3 install flask ujson whatportis xmltodict
+
+#echo "Installing Python 2 dependencies..."
+
+echo "Installing Mozilla Cipher Scanner..."
+git clone https://github.com/mozilla/cipherscan
